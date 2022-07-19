68f85a60cccc6ba3b7066a761d9fabb6b8803bed
Prince Gupta <prince@pixpa.com>
2022-07-20 00:46:42 +0530

    Updates

---
+++ precache-manifest.f8261b638343ea48c43660cc51a6f4b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3dd626457d23084183ac64b8ba98b8",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "9a6fc71aebbb755c50a8",
+    "url": "/reactweb/static/css/main.145a2cec.chunk.css"
+  },
+  {
+    "revision": "af21a7234660ec99f6b0",
+    "url": "/reactweb/static/js/2.cc9a78a9.chunk.js"
+  },
+  {
+    "revision": "9a6fc71aebbb755c50a8",
+    "url": "/reactweb/static/js/main.c553ca33.chunk.js"
+  },
+  {
+    "revision": "a9453479506b1bee5fc3",
+    "url": "/reactweb/static/js/runtime~main.f98dd7bd.js"
+  },
+  {
+    "revision": "f94a693fa782a42e2fb58f29a51bc7c5",
+    "url": "/reactweb/static/media/about.f94a693f.jpg"
+  },
+  {
+    "revision": "ffa111666d4b6478603b5a3ae8275596",
+    "url": "/reactweb/static/media/about1.ffa11166.jpg"
+  },
+  {
+    "revision": "92384b9529f076a5b7db159850869d32",
+    "url": "/reactweb/static/media/avatar.92384b95.png"
+  },
+  {
+    "revision": "445a6635dc31a34f40e84fc56e370605",
+    "url": "/reactweb/static/media/contact.445a6635.png"
+  },
+  {
+    "revision": "6337982a36ff8543491e180edfd138f4",
+    "url": "/reactweb/static/media/herobg.6337982a.jpg"
+  },
+  {
+    "revision": "2af41325df9d266bf75613615494ef62",
+    "url": "/reactweb/static/media/service1.2af41325.png"
+  },
+  {
+    "revision": "cce959eb5e004bbc400de85b917b292c",
+    "url": "/reactweb/static/media/service1.cce959eb.svg"
+  },
+  {
+    "revision": "2c1041515f8fe6336e367bdb4357630c",
+    "url": "/reactweb/static/media/service2.2c104151.svg"
+  },
+  {
+    "revision": "6c71d755d6919020c72b0b1eec6f1dda",
+    "url": "/reactweb/static/media/service3.6c71d755.svg"
+  },
+  {
+    "revision": "7c57553b945a10016304dc6af202112a",
+    "url": "/reactweb/static/media/service6.7c57553b.png"
+  },
+  {
+    "revision": "5d6f0a8fa1bb53828c7e4b41a623961c",
+    "url": "/reactweb/static/media/serviceimg_01.5d6f0a8f.svg"
+  },
+  {
+    "revision": "7c2e228675adc392586e539ffa4d0a7d",
+    "url": "/reactweb/static/media/serviceimg_02.7c2e2286.svg"
+  },
+  {
+    "revision": "e72899d47bc2d494de8ee86560cd441c",
+    "url": "/reactweb/static/media/serviceimg_03.e72899d4.svg"
+  },
+  {
+    "revision": "7a5ba01c42267bb3c55c62e5a3c7a0ba",
+    "url": "/reactweb/static/media/serviceimg_04.7a5ba01c.svg"
+  }
+]);
