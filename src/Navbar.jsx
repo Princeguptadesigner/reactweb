a0faae1e484ea6bd1b6b3c3fd3344f276eb266be
Prince Gupta <prince@pixpa.com>
2022-07-20 00:52:53 +0530

    navbar changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
         <ul className="anchor_nav">
           <li className="current"><a href="#home">Home</a></li>
           <li className=""><a href="#about">About</a></li>
-          <li className=""><a href="#portfolio">Portfolio</a></li>
+          {/* <li className=""><a href="#portfolio">Portfolio</a></li> */}
           <li className=""><a href="#service">Service</a></li>
-          <li className=""><a href="#blog">Blog</a></li>
+          {/* <li className=""><a href="#blog">Blog</a></li> */}
           <li className=""><a href="#contact">Contact</a></li>
         </ul>
       </div>
